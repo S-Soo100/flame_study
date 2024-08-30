4e3fc5e00db45a6c9fc1875d45be89cf3b28a699
백승수 <bss.rol20@gmail.com>
2024-08-30 09:43:30 +0900

    enemy size변경

--- lib/game/airplane_game/game_components/enemy_components/enemy_components_mixin.dart
+++ lib/game/airplane_game/game_components/enemy_components/enemy_components_mixin.dart
@@ -1,18 +1,18 @@
 import 'package:flame/game.dart';
 import 'package:flame_practice/game/airplane_game/airplane_game.dart';
-import 'package:flame_practice/game/airplane_game/game_components/enemy_bullet.dart';
+import 'package:flame_practice/game/airplane_game/game_components/enemy_components/enemy_bullet.dart';
+import 'dart:math' as math;
 
 class EmenyComponentMixin {
   void shoot(AirplaneGame? game, Vector2 position) {
     Vector2 targetPosition = Vector2(game!.size.x / 2, game.size.y);
     Vector2 direction = (targetPosition - position).normalized();
-    // Vector2 bulletPosition = position + direction * game.size.y / 2;
     game.add(EnemnyBullet(position, direction));
   }
 
   Vector2 setComponentSizeByGame(AirplaneGame? game) {
     if (game == null) return Vector2(80, 80);
-    double size = game.size.x / 10;
+    double size = game.size.x / 11;
     return Vector2(size, size);
   }
 
@@ -33,7 +33,7 @@ class EmenyComponentMixin {
   void groupingPattern(AirplaneGame? gameRef, double dt,
       {required double y, required int id, required Vector2 position}) {
     double xStandard = gameRef!.size.x / 8;
-    double _y = gameRef.size.y * 0.2;
+    double _y = gameRef.size.y * 0.1;
     double _x = (xStandard * 2) + (xStandard * id);
     if (id > 4) {
       _y += y + 10;
